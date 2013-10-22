e9d42c0fe1c260629d0a10670f9122d50decb191
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-10-21 20:04:02 -0400

    Added blur effect

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
     // animate the landing text to fade in and move down slightly
     $("#landing_div_text").animate({top: "43%", opacity: 1.0}, 1000, function() {
         // when the text is done animating, animate certain words to a green color
-        $("#landing_div_text span").animate({color:"#5EBA40"}, 1000);
+        $("#landing_div_text span").animate({color:"#5EBA40"}, 1000, function() {
+            $("#landing_image_unblur").animate({opacity:1.0}, 2000);
+            $("#landing_image_blur").animate({opacity:0.0}, 4000);
+            $("#landing_div_text").animate({top: "43%", opacity: 0.0}, 2000);
+        });
     });
     
     // evoked whenever the user scrolls
